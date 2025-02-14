a8a30dd7905b18e46a2513b2cd7921cdd83228de
rasersharpe <jay.bhatt@me.com>
2025-02-14 14:51:20 -0600

    Added footer contents and styled main components.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
-      <div>Footer</div>
+      <div>
+        <p>
+          Created using{" "}
+          <a
+            href="https://rawg.io/apidocs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            [RAWG API]
+          </a>{" "}
+        </p>
+      </div>
+      <div>
+        <p>&copy; 2025 Blake Anderson, Jay Bhatt, Jake Ringate, Kira Ziegler</p>
+      </div>
     </footer>
   );
 };
