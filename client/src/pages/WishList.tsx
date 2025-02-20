8f5d1abecf6fac225a81c5b94bc32affed973937
rasersharpe <jay.bhatt@me.com>
2025-02-19 19:48:52 -0600

    added styles to wishlist

--- client/src/pages/WishList.tsx
+++ client/src/pages/WishList.tsx
@@ -8,7 +8,7 @@ const WishList = () => {
   console.log("User Profile:", AuthService.loggedIn());
   console.log("User ID:", userId);
   console.log("User Profile Data:", AuthService.getProfile());
-  
+
   if (!userId) {
     return <p>Please log in to see your Wishlist.</p>;
   }
@@ -32,7 +32,7 @@ const WishList = () => {
 
   return (
     <div className="games-list">
-      <h1>Wish List</h1>
+      <h2>Wish List</h2>
       {games.length === 0 ? (
         <p>Your wishlist is empty.</p>
       ) : (
